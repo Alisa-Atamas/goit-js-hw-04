cb62a86f4475c713d787b33ea01784ad5e92a644
Alisa <alisaatamas92@gmail.com>
2024-12-17 20:44:00 +0200

    add index file

--- js/task-3.js
+++ js/task-3.js
@@ -1,21 +1,28 @@
-function filterArray(numbers, value) {
-    // Створюємо порожній масив для підходящих чисел
-    const result = [];
-    
-    // Ітерація через кожен елемент масиву
-    for (let i = 0; i < numbers.length; i++) {
-        // Якщо число більше за value, додаємо його до результату
-        if (numbers[i] > value) {
-            result.push(numbers[i]);
-        }
-    }
-    
-    // Повертаємо новий масив з числами, більшими за value
-    return result;
-}
+const profile = {
+    username: "Jacob",
+    playTime: 300,
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+    // Метод для зміни імені користувача
+    changeUsername(newName) {
+        this.username = newName;
+    },
+
+    // Метод для оновлення кількості годин
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+
+    // Метод для отримання інформації про профіль
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    }
+};
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
